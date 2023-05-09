ec65f0b690b7768ab13ddc6be5ba6caf3d026818
leegunwon <125619643+leegunwon@users.noreply.github.com>
2023-05-10 01:40:16 +0900

    Update mycode.py

--- mycode.py
+++ mycode.py
@@ -4,8 +4,7 @@ import numpy as np
 
 class GridWorld():
     def __init__(self):
-        self.result = []
-        self.x_history = [i for i in range(6)]
+        self.x_history = []
         self.counter = 0
 
     def step(self, a):
@@ -23,13 +22,13 @@ class GridWorld():
         return self.x_history, reward, done
 
     def is_done(self):
-        if len(self.x_history) == 6:
+        if self.counter == 6:
             return True
         else:
             return False
 
     def reset(self):
-        self.x_history = [i for i in range(6)]
+        self.x_history = []
         self.counter = 0
         return self.x_history
 
@@ -43,6 +42,8 @@ class QAgent():
         # eps-greedy로 액션을 선택해준다
         num = 0
         x = tuple(s)
+        if x not in self.q_table.keys():
+            self.q_table[x] = [0, 0]
         coin = random.random()
         if coin < self.eps:
             action = random.randint(0, 1)
@@ -55,17 +56,15 @@ class QAgent():
         s, a, r, s_prime = transition
         x = tuple(s)
         x_prime = tuple(s_prime)
-        
-        if x not in  self.q_table.keys():
-          self.q_table[x] = [0, 0]
-        
+
+
         if x_prime not in self.q_table.keys():
-          if len(x_prime) == 6:
-            self.q_table[x_prime] = 0
+            if len(x_prime) == 6:
+                self.q_table[x_prime] = 0
 
         # Q러닝 업데이트 식을 이용
         self.q_table[x][a] = self.q_table[x][a] + 0.1 * (
-                    r + np.amax(self.q_table[x_prime]) - self.q_table[x][a])
+                r + np.amax(self.q_table[x_prime]) - self.q_table[x][a])
 
     def anneal_eps(self):
         self.eps -= 0.01  # Q러닝에선 epsilon 이 좀더 천천히 줄어 들도록 함.
@@ -73,7 +72,7 @@ class QAgent():
 
     def show_table(self):
         q_lst = self.q_table.tolist()
-        data = [0,0,0,0,0,0]
+        data = [0, 0, 0, 0, 0, 0]
         for idx in range(len(q_lst)):
             row = q_lst[idx]
             action = np.argmax(row)
@@ -95,8 +94,10 @@ def main():
             agent.update_table((s, a, r, s_prime))
             s = s_prime
         agent.anneal_eps()
-        if env.x_history == [0,1,0,1,0,0]:
-          agent.q_table[(0,1,0,1,0,0)] += 1000
+        if env.x_history == [0, 1, 0, 1, 0, 0]:
+            agent.q_table[(0, 1, 0, 1, 0, 0)] += 1000
+
+        print(env.x_history)
 
 
 if __name__ == '__main__':
