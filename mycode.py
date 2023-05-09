cdea3328e79ec5b4c3fd27461d784a6ac6c4b1e3
leegunwon <125619643+leegunwon@users.noreply.github.com>
2023-05-10 04:34:45 +0900

    Update mycode.py

--- mycode.py
+++ mycode.py
@@ -1,6 +1,6 @@
 import random
 import numpy as np
-
+import copy
 
 class GridWorld():
     def __init__(self):
@@ -17,6 +17,7 @@ class GridWorld():
         self.x_history.append(a)
         self.counter += 1
 
+        s = tuple(self.x_history)
         done = self.is_done()
 
         return self.x_history, reward, done
@@ -30,26 +31,41 @@ class GridWorld():
     def reset(self):
         self.x_history = []
         self.counter = 0
-        return self.x_history
+        return []
 
 
 class QAgent():
     def __init__(self):
-        self.q_table = {}  # 마찬가지로 Q 테이블을 0으로 초기화
+        self.key = [] 
+        for i in range(2):
+          self.key.append(tuple([i]))
+          for j in range(2):
+            self.key.append(tuple([i, j]))
+            for k in range(2):
+              self.key.append(tuple([i, j, k]))
+              for l in range(2):
+                self.key.append(tuple([i, j, k ,l]))
+                for m in range(2):
+                  self.key.append(tuple([i, j, k ,l, m]))
+                  for n in range(2):
+                    self.key.append(tuple([i, j, k ,l, m, n]))
+
+        self.q_table = {k : [0, 0] for k in self.key} 
+        self.q_table[()] = [0, 0]
+        self.q_table[(0, 1, 0, 1, 0, 0)][0] = 1000
         self.eps = 0.9
 
     def select_action(self, s):
         # eps-greedy로 액션을 선택해준다
-        num = 0
         x = tuple(s)
-        if x not in self.q_table.keys():
-            self.q_table[x] = [0, 0]
+
         coin = random.random()
         if coin < self.eps:
             action = random.randint(0, 1)
         else:
             action_val = self.q_table[x]
             action = np.argmax(action_val)
+
         return action
 
     def update_table(self, transition):
@@ -58,33 +74,27 @@ class QAgent():
         x_prime = tuple(s_prime)
 
 
-        if x_prime not in self.q_table.keys():
-            if len(x_prime) == 6:
-                self.q_table[x_prime] = 0
-
         # Q러닝 업데이트 식을 이용
-        self.q_table[x][a] = self.q_table[x][a] + 0.1 * (
-                r + np.amax(self.q_table[x_prime]) - self.q_table[x][a])
+
+        self.q_table[x][a] = self.q_table[x][a] + 0.1 * (r + np.amax(self.q_table[x_prime]) - self.q_table[x][a])
+
 
     def anneal_eps(self):
-        self.eps -= 0.01  # Q러닝에선 epsilon 이 좀더 천천히 줄어 들도록 함.
+        self.eps -= 0.005  # Q러닝에선 epsilon 이 좀더 천천히 줄어 들도록 함.
         self.eps = max(self.eps, 0.2)
 
     def show_table(self):
-        q_lst = self.q_table.tolist()
-        data = [0, 0, 0, 0, 0, 0]
-        for idx in range(len(q_lst)):
-            row = q_lst[idx]
-            action = np.argmax(row)
-            data[idx] = action
-        print(data)
+        key = list(self.q_table.keys())
+        key.sort()
+        for data in key:
+          print(f"{data} : {self.q_table[data]}")
 
 
 def main():
     env = GridWorld()
     agent = QAgent()
 
-    for n_epi in range(100):
+    for n_epi in range(10000):
         done = False
         s = env.reset()
 
@@ -92,12 +102,10 @@ def main():
             a = agent.select_action(s)
             s_prime, r, done = env.step(a)
             agent.update_table((s, a, r, s_prime))
-            s = s_prime
+            s = copy.deepcopy(s_prime)
+        
         agent.anneal_eps()
-        if env.x_history == [0, 1, 0, 1, 0, 0]:
-            agent.q_table[(0, 1, 0, 1, 0, 0)] += 1000
-
-        print(env.x_history)
+    agent.show_table()
 
 
 if __name__ == '__main__':
