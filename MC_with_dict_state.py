3715a8cbc98d9c71ebd5dae4915dafb3d4f2cf5d
leegunwon <125619643+leegunwon@users.noreply.github.com>
2023-05-19 03:22:31 +0900

    Update MC_with_dict_state.py
    add report with chart

--- MC_with_dict_state.py
+++ MC_with_dict_state.py
@@ -1,7 +1,9 @@
 import random
 import numpy as np
 import copy
-
+import itertools
+import plotly.express as px
+score = [[0, 0], [0, 0],[0, 0],[0, 0], [0, 0], [0, 0]]
 
 class GridWorld():
     def __init__(self):
@@ -42,22 +44,13 @@ class GridWorld():
 
 class QAgent():
     def __init__(self):
-        self.key = []    # 딕셔너리의 키에 해당하는 리스트
-        for i in range(2):
-            self.key.append(tuple([i]))
-            for j in range(2):
-                self.key.append(tuple([i, j]))
-                for k in range(2):
-                    self.key.append(tuple([i, j, k]))
-                    for l in range(2):
-                        self.key.append(tuple([i, j, k, l]))
-                        for m in range(2):
-                            self.key.append(tuple([i, j, k, l, m]))
-                            for n in range(2):
-                                self.key.append(tuple([i, j, k, l, m, n]))
+        self.key = []
+        for i in range(7):
+            self.key += list(itertools.product([0, 1], repeat=i))    # 딕셔너리의 키에 해당하는 리스트
+        self.key.append((0))
+        self.key.append((1))
 
         self.q_table = {k: [0, 0] for k in self.key} # 딕셔너리의 초기값을 0으로 설정
-        self.q_table[()] = [0, 0]   
         self.eps = 0.9
 
     def select_action(self, s):
@@ -82,26 +75,27 @@ class QAgent():
             x_prime = tuple(s_prime)
 
             # 몬테 카를로 방식을 이용하여 업데이트.
+            cum_reward = cum_reward + r
             self.q_table[x][a] = self.q_table[x][a] + 0.01 * (cum_reward - self.q_table[x][a])
-            cum_reward = cum_reward + r 
+
 
 
     def anneal_eps(self):
-        self.eps -= 0.01  
+        self.eps -= 0.005
         self.eps = max(self.eps, 0.1)
 
     def show_table(self):
         key = list(self.q_table.keys())    
         key.sort()
         for data in key:
-            print(f"{data} : 액션 {np.argmax(self.q_table[data])}")
+            print(f"{data} : 액션{self.q_table[data]}")
 
 
 def main():
     env = GridWorld()
     agent = QAgent()
 
-    for n_epi in range(1000):
+    for n_epi in range(3000):
         done = False
         s = env.reset()
         env.u_data = []
@@ -116,10 +110,29 @@ def main():
 
         agent.update_table(env.u_data)
         agent.anneal_eps()
-        print(s)
-    agent.show_table()
+        # print(s)
+    # agent.show_table()
+
+    score[0][0] += agent.q_table[()][0]
+    score[1][0] += agent.q_table[(0,)][0]
+    score[2][0] += agent.q_table[(0, 1)][0]
+    score[3][0] += agent.q_table[(0, 1, 0)][0]
+    score[4][0] += agent.q_table[(0, 1, 0, 1)][0]
+    score[5][0] += agent.q_table[(0, 1, 0, 1, 0)][0]
+    score[0][1] += agent.q_table[()][1]
+    score[1][1] += agent.q_table[(0,)][1]
+    score[2][1] += agent.q_table[(0, 1)][1]
+    score[3][1] += agent.q_table[(0, 1, 0)][1]
+    score[4][1] += agent.q_table[(0, 1, 0, 1)][1]
+    score[5][1] += agent.q_table[(0, 1, 0, 1, 0)][1]
+
 
 
 
 if __name__ == '__main__':
-    main()
+    for i in range(10):
+        main()
+    print(score)
+
+    fig = px.bar(x=['', '0', '01', '010', '0101', '01010'], y=[score[0][0]/10 - score[0][1]/10, score[1][0]/10- score[1][1]/10, score[2][0]/10- score[2][1]/10, score[3][0]/10 -score[3][1]/10, score[4][0]/10 - score[4][1]/10, score[5][0]/10- score[5][1]/10], color=['', '0', '01', '010', '0101', '01010'])
+    fig.show()
